86529feb19d375da50c892193acad9a143d92d35
seriouscatsoserious <121734574+seriouscatsoserious@users.noreply.github.com>
2025-02-01 18:01:55 -0300

    use hashrouter in place of browser router, remove references folder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import './scrollbar.css';
